bab9e7828830425bc7fb3c1f05f418bbe6806375
AidoJ <aidanleo@yahoo.co.uk>
2025-08-14 00:25:33 +1000

    Update index.ts

--- admin/src/components/index.ts
+++ admin/src/components/index.ts
@@ -1,9 +1,6 @@
 export { Header } from "./header";
 export { TherapistPhoto } from "./TherapistPhoto";
-
-export { default as GooglePlacesAddressInput } from './googleplacesaddressinput';
-export { default as TherapistLocationMap } from './therapistlocationmap'; 
-export { default as EnhancedTherapistProfileForm } from './enhancedtherapistprofileform';
-
-// Fix RoleGuard export - check if it's a named export or default export
+export { default as GooglePlacesAddressInput } from './GooglePlacesAddressInput';
+export { default as TherapistLocationMap } from './TherapistLocationMap';
+export { default as EnhancedTherapistProfileForm } from './EnhancedTherapistProfileForm';
 export { RoleGuard } from './RoleGuard';
