9b31caa8af55f851d4427468b113570949ca4a24
AidoJ <aidanleo@yahoo.co.uk>
2025-08-14 00:55:12 +1000

    Update create.tsx

--- admin/src/pages/therapists/create.tsx
+++ admin/src/pages/therapists/create.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router';
 import TherapistProfileManagement from './profile';
 
 export const CreateTherapistPage: React.FC = () => {
   // Just use the same profile component - it handles create mode when no ID is provided
   return <TherapistProfileManagement />;
-};
+}
