94c44fa1b00f097b49b5659d7fe5f33602e41c7e
AidoJ <aidanleo@yahoo.co.uk>
2025-08-13 23:58:55 +1000

    Update index.ts

--- admin/src/components/index.ts
+++ admin/src/components/index.ts
@@ -1,2 +1,10 @@
 export { Header } from "./header";
 export { TherapistPhoto } from "./TherapistPhoto";
+// Add these exports to your existing index.ts file
+export { default as GooglePlacesAddressInput } from './GooglePlacesAddressInput';
+export { default as TherapistLocationMap } from './TherapistLocationMap';
+export { default as EnhancedTherapistProfileForm } from './EnhancedTherapistProfileForm';
+
+// Keep your existing exports too
+export { default as RoleGuard } from './RoleGuard';
+// etc.
