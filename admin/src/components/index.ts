0aef80271e8dd58a78c403f8311ae8c827984f53
AidoJ <aidanleo@yahoo.co.uk>
2025-08-14 00:46:48 +1000

    Update index.ts

--- admin/src/components/index.ts
+++ admin/src/components/index.ts
@@ -1,6 +1,3 @@
 export { Header } from "./header";
 export { TherapistPhoto } from "./TherapistPhoto";
-export { default as GooglePlacesAddressInput } from './GooglePlacesAddressInput';
-export { default as TherapistLocationMap } from './TherapistLocationMap';
-export { default as EnhancedTherapistProfileForm } from './EnhancedTherapistProfileForm';
 export { RoleGuard } from './RoleGuard';
