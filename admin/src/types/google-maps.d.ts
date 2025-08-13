e29512f20ea9828033a6e92f816273bf7b7e420b
AidoJ <aidanleo@yahoo.co.uk>
2025-08-14 00:20:02 +1000

    Update google-maps.d.ts

--- admin/src/types/google-maps.d.ts
+++ admin/src/types/google-maps.d.ts
@@ -1,9 +1,49 @@
 declare global {
   interface Window {
-    google: typeof google;
+    google: any;
   }
 }
 
 declare namespace google.maps {
-  // Add any specific Google Maps types you need
+  class Map {
+    constructor(mapDiv: Element, opts?: any);
+  }
+  class Marker {
+    constructor(opts?: any);
+    setMap(map: Map | null): void;
+  }
+  class Circle {
+    constructor(opts?: any);
+    setMap(map: Map | null): void;
+  }
+  class LatLng {
+    constructor(lat: number, lng: number);
+    lat(): number;
+    lng(): number;
+  }
+  class Geocoder {
+    constructor();
+    geocode(request: any, callback: Function): void;
+  }
+  namespace places {
+    class Autocomplete {
+      constructor(inputField: Element, opts?: any);
+      addListener(eventName: string, handler: Function): any;
+      getPlace(): any;
+    }
+  }
+  namespace event {
+    function addListener(instance: any, eventName: string, handler: Function): any;
+    function clearInstanceListeners(instance: any): void;
+  }
+  interface GeocoderAddressComponent {
+    long_name: string;
+    short_name: string;
+    types: string[];
+  }
+  enum GeocoderStatus {
+    OK = 'OK'
+  }
 }
+
+export {};
